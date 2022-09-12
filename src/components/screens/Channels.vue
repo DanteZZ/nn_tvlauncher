de2f3ac9a74baf2ba740022f1803fd203e2ca2e6
Maxim Trofimov <dantezzstudio@gmail.com>
2022-09-12 16:27:52 +0700

    Rfyfks

--- src/components/screens/Channels.vue
+++ src/components/screens/Channels.vue
@@ -1,25 +1,12 @@
 <template>
   <div>
     <h1>Список каналов</h1>
-    <video-player :options="videoOptions"></video-player>
   </div>
 </template>
 <script>
 export default {
   name: "screen-channels",
   props: {},
-  data() {
-    return {
-      videoOptions: {
-        source: {
-          type: "application/x-mpegURL",
-          src: "http://109.248.63.9:1013/sts/index.m3u8",
-          withCredentials: false,
-        },
-        live: true,
-      },
-    };
-  },
   methods: {},
 };
 </script>
